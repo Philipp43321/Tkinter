507159bc88af26d61c46446a13b50f9dd4f290fa
Philipp43321 <markachev.philipp@gmail.com>
2025-01-21 18:19:32 +0100

    Add files via upload

--- progressbar.py
+++ progressbar.py
@@ -4,4 +4,30 @@ screen = Tk()
 screen.geometry("300x100")
 bar = Progressbar(screen,orient=HORIZONTAL,length=200,mode="determinate")
 bar.pack()
+def progress():
+    import time
+    bar["value"]=20
+    screen.update_idletasks()
+    time.sleep(1)
+
+    bar["value"]=40
+    screen.update_idletasks()
+    time.sleep(1)
+    
+    bar["value"]=60
+    screen.update_idletasks()
+    time.sleep(1)
+
+    bar["value"]=80
+    screen.update_idletasks()
+    time.sleep(1)
+
+    bar["value"]=100
+
+button = Button(screen,text="Press Me",command=progress)
+button.pack()
+
+#spinbox
+spinbox=Spinbox(screen,from_=0,to=20)
+spinbox.pack()
 screen.mainloop()
