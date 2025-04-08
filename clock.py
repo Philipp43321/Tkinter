5fefe75c295a10a53e04928acd68c814a2a0e742
Philipp43321 <markachev.philipp@gmail.com>
2025-04-08 18:44:37 +0200

    Add files via upload

--- clock.py
+++ clock.py
@@ -5,7 +5,12 @@ from time import strftime
 screen = Tk()
 screen.title("Clock")
 
+def time():
+    string = strftime('%H:%M:%S %p')
+    clock.config(text=string)
+    clock.after(1000,time)
 
-clock = Label(screen,font=("arial",120),text="12:56:23",background="blue",foreground="black")
+clock = Label(screen,font=("arial",120),background="blue",foreground="black")
 clock.pack(anchor="center")
+time()
 screen.mainloop()
