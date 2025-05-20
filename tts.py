1d6af4cb1697000eb845e677066cd3537e677232
Philipp43321 <markachev.philipp@gmail.com>
2025-05-20 18:54:43 +0200

    Update tts.py

--- tts.py
+++ tts.py
@@ -10,8 +10,13 @@ frame1.pack(fill = X)
 frame2 = Frame(screen,bg="green",height=300)
 frame2.pack(fill=X)
 
-label = Label(frame1,text="TEXT TO SPEECH",font="bold,300",bg="white")
-label.place(x=200,y=100)
+label = Label(frame1,text="TEXT TO SPEECH",font=("Arial",30,"bold"),bg="white")
+label.place(x=100,y=100)
 
+entry = Entry(frame2,width=30,bd=5,font=15)
+entry.place(x=130,y=50)
+
+button = Button(frame2,text="CONVERT",font=("Arial",12,"bold"),bg="red")
+button.place(x=220,y=125)
 
 screen.mainloop()
