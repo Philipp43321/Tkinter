14d071b36793ce5d76d13b989d3be6b6bb3f3ac6
Philipp43321 <markachev.philipp@gmail.com>
2025-05-20 18:31:03 +0200

    Update memoriser.py

--- memoriser.py
+++ memoriser.py
@@ -1,19 +1,48 @@
 from tkinter import*
 from tkinter.filedialog import *
 screen = Tk()
+
+#functions 
+
+def funadd():
+    listbox.insert(END,entry.get())
+    entry.delete(0,END)
+
+def deletefun():
+    selection = listbox.curselection()
+    listbox.delete(selection)
+
+def savefun():
+    file1 = asksaveasfile(defaultextension=".txt")
+    if file1 is not None:
+        for i in listbox.get(0,END):
+            print(i.strip(),file=file1)
+        listbox.delete(0,END)
+
+def openfun():
+    file2 = askopenfile(title="which file would you like to open?")
+    if file2 is not None:
+        listbox.delete(0,END)
+        newfile = file2.readlines()
+        for i in newfile:
+            listbox.insert(END,i.strip())
+
+
+
+
 frame = Frame(screen)
 bframe = Frame(screen)
 bframe.pack(side=TOP,anchor="w",padx=10,pady=5)
 screen.title("Open/Delete file")
-save = Button(bframe,text = "Save")
+save = Button(bframe,text = "Save",command=savefun)
 save.pack(side=LEFT,pady = 5)
 entry = Entry(screen,width=10)
 entry.pack(side=TOP,pady = 5)
-add = Button(bframe,text = "Add")
+add = Button(bframe,text = "Add",command=funadd)
 add.pack(side=LEFT,pady = 5)
-open = Button(bframe,text = "Open")
+open = Button(bframe,text = "Open",command = openfun)
 open.pack(side=LEFT,pady = 5)
-delete = Button(bframe,text = "Delete")
+delete = Button(bframe,text = "Delete",command=deletefun)
 delete.pack(side=LEFT,pady = 5)
 
 
